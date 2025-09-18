70e09dfb009450f559d91d9504274f13e967e8bc
bakje <59623580+bakjedev@users.noreply.github.com>
2025-09-18 18:40:32 +0200

    Change gdscript to use textures that are loaded

--- demo/csg_box_3d.gd
+++ demo/csg_box_3d.gd
@@ -1,5 +1,26 @@
 extends CSGBox3D
 
+var paths = []
+func _ready():
+	for n in 10:
+		paths.append("res://textures/noise/texture_%d.png" % n)
+	AssetLoader.initialize({
+		"distribution": AssetLoader.DIST_CUSTOM,
+		"pools": [
+			{"type": "mesh", "count": 2, "priority": Thread.PRIORITY_HIGH},
+			{"type": "texture", "count": 2, "priority": Thread.PRIORITY_NORMAL},
+		]
+	})
+
+func done_loading(resource: Resource, path: String, error: int ):
+	print("yeahh ", path, " ", error, " ", resource.get_rid())
+	material.albedo_texture = resource as Texture
+	
+
 func _physics_process(delta: float) -> void:
 	if self.position.x < 5.0:
 		self.position += Vector3(delta, 0, 0)
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		AssetLoader.load_batch(paths, done_loading, Thread.PRIORITY_NORMAL, "texture")
