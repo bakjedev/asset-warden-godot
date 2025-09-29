c492945c79878e2ce8b7bab0404952fd98bb6a76
bakje <59623580+bakjedev@users.noreply.github.com>
2025-09-29 13:28:29 +0200

    Make world button load and create all the terrain meshes

--- demo/world.gd
+++ demo/world.gd
@@ -1,6 +1,12 @@
 extends Node3D
 
 @export var load_button: Button
+@export var progress_label: Label
+
+var batch_id: int = -1
+var done: bool = false
+var loaded_count: int = 0
+var are_meshes: bool = false
 
 func _ready():
 	AssetLoader.initialize({
@@ -14,5 +20,38 @@ func _ready():
 	if load_button:
 		load_button.pressed.connect(_on_load_button_pressed)
 
+func done_loading(_res: Array):
+	print("DONE!")
+	done = true
+	loaded_count = _res.size()
+	if _res[0] is Mesh and not are_meshes:
+		are_meshes = true
+	
+	var spacing = 10.0
+	var index = 0
+	
+	for resource in _res:
+		var mesh = resource as Mesh
+		var mesh_instance = MeshInstance3D.new()
+		mesh_instance.mesh = mesh
+		var x_pos = (index % 16) * spacing
+		var z_pos = (index / 16.0) * spacing
+		mesh_instance.position = Vector3(x_pos, 0, z_pos)
+		mesh_instance.rotation_degrees = Vector3(180, 0, 0)
+		add_child(mesh_instance)
+		index += 1
+
+
 func _on_load_button_pressed():
-	print("YEAH!")
+	var paths = []
+	for n in 256:
+		paths.append("res://sub100world/terrain_sub100_%d.obj" % n)
+	batch_id = AssetLoader.load_batch(paths, done_loading,Thread.PRIORITY_NORMAL, "mesh")
+
+func _process(_delta):
+	if done:
+		progress_label.text = "YES DONE " + String.num(loaded_count) + " Are Meshes? " + String.num(are_meshes)
+	else:
+		var status = AssetLoader.status_batch(batch_id)
+		var progress = AssetLoader.progress_batch(batch_id)
+		progress_label.text = "Status: " + String.num(status) + " Progress: " + String.num(progress)
